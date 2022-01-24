67e5c332d5c8ce5d630eaf70349f0fdfd8234b78
ARPIT GUPTA <ARPITGUPTA1014@gmail.com>
2022-01-24 20:50:05 +0530

    Adding code

---
+++ Hackerrank/Sets-STL.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    int t;
+    cin>>t;
+    set<ll> st;
+    while(t--){
+        ll y,x;
+        cin>>y>>x;
+        if(y==1){
+            st.insert(x);
+        }
+        else if(y==2){
+            st.erase(x);
+        }
+        else if(y==3){
+            auto it=st.find(x);
+            if(it==st.end()){
+                cout<<"No"<<endl;
+            }
+            else{
+                cout<<"Yes"<<endl;
+            }
+        }
+    }
+    return 0;
+}
