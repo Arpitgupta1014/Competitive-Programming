22f8d48fd97ab66b99d80e1b20ca9d9d3082c148
ARPIT GUPTA <ARPITGUPTA1014@gmail.com>
2022-01-30 17:08:37 +0530

    Adding code

---
+++ Header/Short.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <chrono>
+#include <map>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <set>
+#include <string>
+#define ll long long int
+#define mod 1000000007
+#define modd 998244353
+#define INF 1e9+10
+#define FAST
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+#define LOOP(i, a, b) for (int i = a; i <= b; i++)
+#define LOP(i, a, b, c) for (int i = a; i < b; i += c)
+#define BACKLOOP(i, a, b) for (int i = a; i >= b; i--)
+#define all(v) (v).begin(), (v).end()
+#define inp(a) cin >> a
+#define VI vector<int>
+#define VL vector<ll>
+#define PII pair<int, int>
+#define PLL pair<ll, ll>
+#define VPII vector<pair<int, int>>
+#define VPLL vector<pair<ll, ll>>
+#define PB push_back
+#define PP pop_bacK
+#define ff first
+#define ss second
+using namespace std;
