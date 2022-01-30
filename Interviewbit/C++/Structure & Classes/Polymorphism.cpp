8adbb5bb2185f5cea57e3780d63ef36695741a75
ARPIT GUPTA <ARPITGUPTA1014@gmail.com>
2022-01-30 19:33:35 +0530

    Adding code

---
+++ Interviewbit/C++/Structure & Classes/Polymorphism.cpp
@@ -0,0 +1,25 @@
+/*
+#include<iostream>
+using namespace std;
+*/
+
+// YOUR CODE GOES HERE
+int compute(int x,int y){
+    return abs(x-y);
+}
+
+int compute(int z){
+    return z*z;
+}
+
+/*
+int main()  {
+    int x,y;
+    cin>>x>>y;
+    cout<<compute(x,y)<<endl;
+    int z;
+    cin>>z;
+    cout<<compute(z)<<endl;
+    return 0;
+}
+*/
