a518edc1e5fa0e343cf755a079dcdc276ec1f152
ARPIT GUPTA <ARPITGUPTA1014@gmail.com>
2022-02-11 23:45:06 +0530

    Adding code

--- Leetcode/contains-duplicate.cpp
+++ Leetcode/contains-duplicate.cpp
@@ -10,3 +10,16 @@ public:
         return false;
     }
 };
+// Second code
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+            for(int i=0;i<nums.size()-1;++i){
+                if(nums[i]==nums[i+1]){
+                    return true;
+                }
+            }
+        return false;
+    }
+};
