d4c65da2305a80fa7a112bfe1eb0aacab260e06c
ARPIT GUPTA <ARPITGUPTA1014@gmail.com>
2022-03-05 19:15:33 +0530

    Adding code

---
+++ Codeforces/1646A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t; 
+	cin >> t;
+	while(t--){
+		long long n, s; 
+		cin>>n>> s;
+		cout << s / (n * n) <<endl;
+	}
+	return 0;
+}
